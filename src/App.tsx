7e0e2cc8369180d77e4e8cf0db09e4c74b80f6d7
龍門渕透華 <swearl@163.com>
2021-04-24 15:59:26 +0800

    配置

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { defineComponent, h } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    return () => <img alt="Vue logo" src="@/assets/logo.png" />;
+  },
+});
