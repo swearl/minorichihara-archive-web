436e57caa852ab5bb0a2ce33d3bd5ebfc2815556
龍門渕透華 <swearl@163.com>
2021-04-25 22:50:23 +0800

    更新配置

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,12 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJSX()],
-  alias: {
-    '@': path.resolve(__dirname, './src'),
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
+      },
+    ],
   },
 });
